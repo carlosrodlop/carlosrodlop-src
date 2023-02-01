316de0a898d4a6d745d361d3287b836e476a1b51
Carlos Rodriguez Lopez <it.carlosrodlop@gmail.com>
2023-02-01 16:01:33 +0100

    gist movement

--- snippets/ldapsearch.sh
+++
@@ -1,8 +0,0 @@
-# https://github.com/rroemhild/docker-test-openldap
-# Full tree
-ldapsearch -LLL -H ldap://localhost:389 -M -b "dc=planetexpress,dc=com" -D "cn=admin,dc=planetexpress,dc=com" -w "GoodNewsEveryone"
-# ou=people
-ldapsearch -LLL -H ldap://127.0.0.1:389 -M -b "dc=planetexpress,dc=com" -D "cn=admin,dc=planetexpress,dc=com" -w "GoodNewsEveryone" ou="people"
-# professor
-ldapsearch -LLL -H ldap://127.0.0.1:389 -M -b "dc=planetexpress,dc=com" -D "cn=admin,dc=planetexpress,dc=com" -w "GoodNewsEveryone" uid="zoidberg"
-ldapsearch -LLL -H ldap://127.0.0.1:389 -M -b "dc=planetexpress,dc=com" -D "cn=admin,dc=planetexpress,dc=com" -w "GoodNewsEveryone" cn="admin_staff"
